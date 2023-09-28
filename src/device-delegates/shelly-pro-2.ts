569686a43ee837625c2dca7f150702fe613c5d33
Jared Solomon <jmsolomon2000@gmail.com>
2023-09-28 11:49:54 -0400

    Added missing Pro 2 model number

--- src/device-delegates/shelly-pro-2.ts
+++ src/device-delegates/shelly-pro-2.ts
@@ -12,15 +12,21 @@ import { DeviceDelegate } from './base';
 export class ShellyPro2Delegate extends DeviceDelegate {
   protected setup() {
     const d = this.device as ShellyPro2;
-
     this.addSwitch(d.switch0);
     this.addSwitch(d.switch1);
   }
 }
+
+export class ShellyPro2Rev2UL extends ShellyPro2Rev2 {
+  static readonly model: string = 'SPSW-202XE12UL';
+}
+
+ShellyPro2.registerClass(ShellyPro2Rev2UL);
 
 DeviceDelegate.registerDelegate(
   ShellyPro2Delegate,
   ShellyPro2,
   ShellyPro2Rev1,
   ShellyPro2Rev2,
+  ShellyPro2Rev2UL,
 );
